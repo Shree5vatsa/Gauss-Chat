47c13a41b3f7ec7992e8486a275b246e54322a53
Shree5vatsa <shreevatsa9124@gmail.com>
2026-02-16 15:31:32 +0530

    bug fixes and backend is ready to deploy

--- backend/src/controllers/chats.controller.ts
+++ backend/src/controllers/chats.controller.ts
@@ -2,55 +2,58 @@ import { asyncHandler } from "../middlewares/asyncHandler.middleware";
 import { Request, Response } from "express";
 import { UnauthorizedException } from "../utils/app-Error";
 import { chatIdSchema, createChatSchema } from "../validators/chat.validator";
-import { createChatService, getUserChatService, getSingleChatService } from "../services/chat.service";
+import {
+  createChatService,
+  getUserChatService,
+  getSingleChatService,
+} from "../services/chat.service";
 import { HTTP_STATUS } from "../config/http.config";
 
 export const createChatController = asyncHandler(
-    async(req:Request,res:Response)=>{
-        const userId = req.user?._id;
-        if (!userId) {
-            throw new UnauthorizedException("User not authenticated");
-        }
-        const body = createChatSchema.parse(req.body);
-        
-        const chat = await createChatService(userId, body);
-
-        return res.status(HTTP_STATUS.CREATED).json({
-            message: "Chat created successfully",
-            chat,
-        });
-
+  async (req: Request, res: Response) => {
+    const userId = req.user?._id?.toString();
+    if (!userId) {
+      throw new UnauthorizedException("User not authenticated");
     }
-)
+    const body = createChatSchema.parse(req.body);
 
+    const chat = await createChatService(userId, body);
+
+    return res.status(HTTP_STATUS.CREATED).json({
+      message: "Chat created successfully",
+      chat,
+    });
+  },
+);
 
 export const getUserChatController = asyncHandler(
-    async (req: Request, res: Response) => {
-        const userId = req.user?._id;
-        if (!userId) {
-            throw new UnauthorizedException("User not authenticated");
-        }
-        const chats = await getUserChatService(userId);
-
-        return res.status(HTTP_STATUS.OK).json({
-            message: "User Chats fetched successfully",
-            chats,
-        })
+  async (req: Request, res: Response) => {
+    const userId = req.user?._id?.toString();
+    if (!userId) {
+      throw new UnauthorizedException("User not authenticated");
     }
-)
+    const chats = await getUserChatService(userId);
+
+    return res.status(HTTP_STATUS.OK).json({
+      message: "User Chats fetched successfully",
+      chats,
+    });
+  },
+);
 
 export const getSingleChatController = asyncHandler(
-    async (req: Request, res: Response)=>{
-        const userId = req.user?._id;
-        
-        const { id } = chatIdSchema.parse(req.params);
-        
-        const {chat, messages} = await getSingleChatService(id, userId);
-        
-        return res.status(HTTP_STATUS.OK).json({
-            message: "Chat fetched successfully",
-            chat,
-            messages,
-        });
-    }
-)
+  async (req: Request, res: Response) => {
+    const userId = req.user?._id?.toString();
+    if (!userId) throw new UnauthorizedException("User not authenticated");
+
+    const { id } = chatIdSchema.parse(req.params);
+
+    const { chat, messages } = await getSingleChatService(id, userId);
+
+    return res.status(HTTP_STATUS.OK).json({
+      message: "Chat fetched successfully",
+      chat,
+      messages,
+    });
+  },
+);
--- backend/src/models/chat.model.ts
+++ backend/src/models/chat.model.ts
@@ -1,73 +1,70 @@
-import mongoose,{Document,Schema} from "mongoose";
+import mongoose, { Document, Schema } from "mongoose";
 
-
-export interface ChatDocument extends Document{
-    participants: mongoose.Types.ObjectId[];
-    isGroup: boolean;
-    groupName?: string;
-    admin?: mongoose.Types.ObjectId;
-    lastMessage?: mongoose.Types.ObjectId | null;
-    createdAt: Date;
-    updatedAt: Date;
+export interface ChatDocument extends Document {
+  participants: mongoose.Types.ObjectId[];
+  isGroup: boolean;
+  groupName?: string;
+  admin?: mongoose.Types.ObjectId;
+  lastMessage?: mongoose.Types.ObjectId | null;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-const chatSchema = new Schema<ChatDocument>({
+const chatSchema = new Schema<ChatDocument>(
+  {
     //participants
     participants: {
-        type: [Schema.Types.ObjectId],
-        ref: "User",
-        required: true,
+      type: [Schema.Types.ObjectId],
+      ref: "User",
+      required: true,
 
-        //mongoose.Types.ObjectId[] gives the run time data
-        validate: {
-            validator: function (value: mongoose.Types.ObjectId[]) {
-                return value.length >= 2;
-            },
-            message: "Chat must have at least two participants"
-        }
+      //mongoose.Types.ObjectId[] gives the run time data
+      validate: {
+        validator: function (value: mongoose.Types.ObjectId[]) {
+          return value.length >= 2;
+        },
+        message: "Chat must have at least two participants",
+      },
     },
 
     isGroup: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
     groupName: {
-        type: String,
-        trim: true,
-        required: function (this: ChatDocument) {
-            return this.isGroup;
-        }
+      type: String,
+      trim: true,
+      required: function (this: ChatDocument) {
+        return this.isGroup;
+      },
     },
     admin: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-        required: function (this: ChatDocument) {
-            return this.isGroup;
-        },
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: function (this: ChatDocument) {
+        return this.isGroup;
+      },
     },
     lastMessage: {
-        type: Schema.Types.ObjectId,
-        ref: "Message",
-        default: null,
-    }
-},
-    { timestamps: true }
+      type: Schema.Types.ObjectId,
+      ref: "Message",
+      default: null,
+    },
+  },
+  { timestamps: true },
 );
-
-chatSchema.index({ participants: 1 });
 
 chatSchema.index({ participants: 1, updatedAt: -1 });
 
 chatSchema.index(
-    { participants: 1 },
-    {
-        unique: true,
-        partialFilterExpression: {
-            isGroup: false,
-        }
-    }
+  { participants: 1 },
+  {
+    unique: true,
+    partialFilterExpression: {
+      isGroup: false,
+    },
+  },
 );
-
 
 const ChatModel = mongoose.model<ChatDocument>("Chat", chatSchema);
 
